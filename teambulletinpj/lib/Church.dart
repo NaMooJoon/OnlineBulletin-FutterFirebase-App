c6f06b6234e07acceb6254876512807570d1c3b7
ParkByungYoon <21800271@handong.edu>
2021-11-28 21:40:10 +0900

    교회 클래스 교회 정보가 담긴 클래스, 선언을 통해 Object로 사용가능

---
+++ teambulletinpj/lib/Church.dart
@@ -0,0 +1,24 @@
+class Church {
+  const Church({
+    required this.id,
+    required this.churchName,
+    required this.location,
+    required this.imageURL,
+    required this.master,
+    required this.pastor,
+    required this.memberList,
+    required this.createdAt,
+  });
+
+  final String id;
+  final String churchName;
+  final String location;
+  final String imageURL;
+  final String master;
+  final String pastor;
+  final List<String> memberList;
+  //final Timestamp createdAt;
+
+  @override
+  String toString() => churchName;
+}
