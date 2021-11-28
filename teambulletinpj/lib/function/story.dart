aa21e7f21862dd96484167f6e3a8f9edc8d38a74
ParkByungYoon <21800271@handong.edu>
2021-11-29 03:32:30 +0900

    교회 소식을 담는 story 페이지 추가 기존 info page에 존재하던 코드를 기반으로  이름이 변경되었음

---
+++ teambulletinpj/lib/function/story.dart
@@ -0,0 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shrine/function/storystruct.dart';
+
+import 'infostories.dart';
+
+class StoryPage extends StatefulWidget {
+  StoryPage({Key? key}) : super(key: key);
+
+  @override
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  List<Story> story = [];
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: FirebaseFirestore.instance.collection('infostory').get(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        story = snapshot.data!.docs.map<Story>((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+          return Story(
+            username: data['name'],
+            contentimgUrl: data['contentimgUrl'],
+            updatetime: data['updatetime'],
+            content: data['content'],
+            userimgUrl: data['userimgUrl'],
+            likeUsers: data['likeUsers'].length,
+            likeUserList: data['likeUsers'],
+            docid: document.id,
+            uid: data['uid'],
+          );
+        }).toList();
+
+        return CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/login');
+                },
+                child: Text("Back"),
+              ),
+              backgroundColor: Colors.transparent,
+              centerTitle: true,
+              elevation: 0.0,
+              iconTheme: IconThemeData(color: Colors.black),
+              title: Text(
+                '교회 소식',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                  final Story post = story[index];
+                  return PostContainer(post: post);
+                },
+                childCount: story.length,
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
