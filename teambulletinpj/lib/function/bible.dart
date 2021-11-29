94aa1f4053e4fc2db5bd567f574a0c2e73cebab7
ParkByungYoon <21800271@handong.edu>
2021-11-29 23:27:21 +0900

    성경, 찬송가 DB 연동 완료

--- teambulletinpj/lib/function/bible.dart
+++ teambulletinpj/lib/function/bible.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
@@ -28,112 +30,129 @@ class _BiblePageState extends State<BiblePage> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 10,),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: ToggleButtons(
-                color: Colors.black26,
-                constraints: BoxConstraints(minWidth: 175.0, minHeight: 40.0),
-                children: <Widget>[
-                  Text("구약", style:TextStyle(fontSize: 17, fontWeight: FontWeight.bold,), ),
-                  Text("신약", style:TextStyle(fontSize: 17, fontWeight: FontWeight.bold,), ),
-                ],
-                onPressed: (int index) {
-                  setState(() {
-                    for(int i=0; i<2; i++){_selection[i] = false;}
-                    _selection[index] = true;
-                  });
-                },
-                isSelected: _selection,
-                borderRadius: BorderRadius.circular(20),
-                fillColor: Colors.white,
-                selectedColor: Colors.green,
-              ),
-            ),
-            const SizedBox(height: 25.0),
-            Divider(thickness: 2.0,),
-            Expanded(
-              child: Row(
-                children: [
-                  Flexible(
-                    child: ListView.builder(
-                      padding: const EdgeInsets.all(0.0),
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: 10,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          child: Column(
-                            children: [
-                              ListTile(
-                                title: _firstIndex == index ?
-                                  Text("창세기", style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
-                                  Text("창세기", style:TextStyle(fontSize: 15, color: Colors.black26), ),
-                                onTap: () {
-                                  setState(() {
-                                    _firstIndex = index;
-                                    _secondIndex = -1;
-                                  });
-                                },
-                                contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
-                                dense:true,
+      body: FutureBuilder<QuerySnapshot>(
+          future:  FirebaseFirestore.instance.collection('bible')
+              .orderBy("rowNumber", descending: false).get(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return  Text('');
+            }
+            final documents = snapshot.data!.docs;
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 10,),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ToggleButtons(
+                    color: Colors.black26,
+                    constraints: BoxConstraints(minWidth: 175.0, minHeight: 40.0),
+                    children: <Widget>[
+                      Text("구약", style:TextStyle(fontSize: 17, fontWeight: FontWeight.bold,), ),
+                      Text("신약", style:TextStyle(fontSize: 17, fontWeight: FontWeight.bold,), ),
+                    ],
+                    onPressed: (int index) {
+                      setState(() {
+                        for(int i=0; i<2; i++){_selection[i] = false;}
+                        _selection[index] = true;
+                      });
+                    },
+                    isSelected: _selection,
+                    borderRadius: BorderRadius.circular(20),
+                    fillColor: Colors.white,
+                    selectedColor: Colors.green,
+                  ),
+                ),
+                const SizedBox(height: 25.0),
+                Divider(thickness: 2.0,),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: ListView.builder(
+                          padding: const EdgeInsets.all(0.0),
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: documents.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Column(
+                                  children: [
+                                    ListTile(
+                                      title: _firstIndex == index ?
+                                      Text(documents[index]['chapterName'], style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
+                                      Text(documents[index]['chapterName'], style:TextStyle(fontSize: 15, color: Colors.black26), ),
+                                      onTap: () {
+                                        setState(() {
+                                          _firstIndex = index;
+                                          _secondIndex = -1;
+                                        });
+                                      },
+                                      contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
+                                      dense:true,
+                                    ),
+                                    Divider(thickness: 1.0,)]
                               ),
-                              Divider(thickness: 1.0,)]
-                          ),
-                        );
-                      },
-                    ),
+                            );
+                          },
+                        ),
+                      ),
+                      VerticalDivider( thickness: 1,),
+                      Flexible(
+                        child: ListView.builder(
+                          padding: const EdgeInsets.all(0.0),
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: documents[_firstIndex]['chapterNum'],
+                          itemBuilder: (BuildContext context, int index) {
+                            int idx = index+1;
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    ListTile(
+                                      title: _secondIndex == index ?
+                                      Text("$idx장", style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
+                                      Text("$idx장", style:TextStyle(fontSize: 15, color: Colors.black26), ),
+                                      onTap: () {
+                                        setState(() {
+                                          _secondIndex = index;
+                                        });
+                                        /*  // 오류 방지를 위해 //
+                                        int chapter = _secondIndex + 1;
+                                        String chapterName = documents[_firstIndex]['chapterName'];
+                                        */
+                                        int chapter = 1;
+                                        String chapterName = 'Genesis';
+                                        Navigator.of(context).push (
+                                          MaterialPageRoute(
+                                            builder: (BuildContext context) => PlayerPage(chapterName, chapter),
+                                          ),
+                                        );
+                                      },
+                                      contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
+                                      dense:true,
+                                    ),
+                                    Divider(thickness: 1.0,)]
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                  VerticalDivider( thickness: 1,),
-                  Flexible(
-                    child: ListView.builder(
-                      padding: const EdgeInsets.all(0.0),
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: 50,
-                      itemBuilder: (BuildContext context, int index) {
-                        int idx = index+1;
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          child: Column(
-                              children: [
-                                ListTile(
-                                  title: _secondIndex == index ?
-                                  Text("$idx장", style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
-                                  Text("$idx장", style:TextStyle(fontSize: 15, color: Colors.black26), ),
-                                  onTap: () {
-                                    setState(() {
-                                      _secondIndex = index;
-                                    });
-                                    Navigator.of(context).push (
-                                      MaterialPageRoute(
-                                        builder: (BuildContext context) => PlayerPage('$_firstIndex,$_secondIndex'),
-                                      ),
-                                    );
-                                  },
-                                  contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
-                                  dense:true,
-                                ),
-                                Divider(thickness: 1.0,)]
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ]
             ),
-          ]
-        ),
+          );
+        }
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
@@ -141,104 +160,109 @@ class _BiblePageState extends State<BiblePage> {
 
 
 class PlayerPage extends StatefulWidget {
-  final String _bibleID;
+  final String chapterName;
+  final int chapter;
 
-  PlayerPage(this._bibleID);
+  PlayerPage(this.chapterName, this.chapter);
 
   @override
-  _PlayerPageState createState() => _PlayerPageState(_bibleID);
+  _PlayerPageState createState() => _PlayerPageState(chapterName, chapter);
 }
 
 class _PlayerPageState extends State<PlayerPage> {
-  String _bibleID;
-  _PlayerPageState(this._bibleID);
+  String chapterName;
+  int chapter;
+  _PlayerPageState(this.chapterName, this.chapter);
   int _selectedIndex = 0;
-
-  late YoutubePlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = YoutubePlayerController(
-      initialVideoId:
-        YoutubePlayer.convertUrlToId
-          ('https://www.youtube.com/watch?v=2-2dyh2JysM&ab_channel='
-            '%EA%B3%B5%EB%8F%99%EC%B2%B4%EC%84%B1%EA%B2%BD%EC%9D%BD%EA%B8%B0')!,
-      flags: YoutubePlayerFlags(
-        mute: false,
-        autoPlay: true,
-        disableDragSeek: true,
-        loop: false,
-        enableCaption: false,
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black),
-        title: Text('성경',
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance.collection('bible').doc(chapterName)
+              .collection('CH$chapter').get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return  Text('');
+        }
+        final documents = snapshot.data!.docs;
+
+        YoutubePlayerController _controller =YoutubePlayerController(
+          initialVideoId:
+          YoutubePlayer.convertUrlToId(documents[chapter-1]['videoURL'])!,
+          flags: YoutubePlayerFlags(
+            mute: false,
+            autoPlay: true,
+            disableDragSeek: true,
+            loop: false,
+            enableCaption: false,
           ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              YoutubePlayer(
-                controller: _controller,
-                bottomActions: [
-                  CurrentPosition(),
-                  const SizedBox(width: 10.0),
-                  ProgressBar(isExpanded: true),
-                  const SizedBox(width: 10.0),
-                  RemainingDuration(),
-                  //FullScreenButton(),
-                ],
-                showVideoProgressIndicator: true,
-                progressIndicatorColor: Colors.white,
+        );
+
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            elevation: 0.0,
+            iconTheme: IconThemeData(color: Colors.black),
+            title: Text('성경',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black
               ),
-              Expanded(
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(top:10.0),
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: 10,
-                  itemBuilder: (BuildContext context, int index) {
-                    int idx = index + 1;
-                    return ListTile(
-                      minLeadingWidth : 20,
-                      leading: _selectedIndex == index ?
-                      Text("$idx", style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
-                      Text("$idx", style:TextStyle(fontSize: 15, color: Colors.black26), ),
-                      onTap: () {
-                        setState(() {
-                          _selectedIndex = index;
-                        });
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  YoutubePlayer(
+                    controller: _controller,
+                    bottomActions: [
+                      CurrentPosition(),
+                      const SizedBox(width: 10.0),
+                      ProgressBar(isExpanded: true),
+                      const SizedBox(width: 10.0),
+                      RemainingDuration(),
+                      //FullScreenButton(),
+                    ],
+                    showVideoProgressIndicator: true,
+                    progressIndicatorColor: Colors.white,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.only(top:10.0),
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        int idx = index + 1;
+                        return ListTile(
+                          minLeadingWidth : 20,
+                          leading: _selectedIndex == index ?
+                          Text("$idx", style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
+                          Text("$idx", style:TextStyle(fontSize: 15, color: Colors.black26), ),
+                          onTap: () {
+                            setState(() {
+                              _selectedIndex = index;
+                            });
+                          },
+                          title: _selectedIndex == index ?
+                          Text(documents[chapter-1]['verse'][index], style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
+                          Text(documents[chapter-1]['verse'][index], style:TextStyle(fontSize: 15, color: Colors.black26), ),
+                          contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
+                          dense:true,
+                        );
                       },
-                      title: _selectedIndex == index ?
-                      Text("태초에 하니님이 천지를 창조하시니라", style:TextStyle(fontSize: 15, fontWeight: FontWeight.bold), ) :
-                      Text("태초에 하니님이 천지를 창조하시니라", style:TextStyle(fontSize: 15, color: Colors.black26), ),
-                      contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
-                      dense:true,
-                    );
-                  },
-                ),
+                    ),
+                  ),
+                ]
               ),
-            ]
+            ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
