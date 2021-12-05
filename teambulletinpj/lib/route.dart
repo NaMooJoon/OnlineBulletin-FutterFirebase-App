c314eeaae1dc39ed4d72d9e9bd87e6b3c69033ba
NaMooJoon <47983023+NaMooJoon@users.noreply.github.com>
2021-12-06 03:41:03 +0900

    REMOVE comment

--- teambulletinpj/lib/route.dart
+++ teambulletinpj/lib/route.dart
@@ -1,17 +1,3 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
 import 'package:shrine/function/addinfo.dart';
 
@@ -24,7 +10,6 @@ import 'login.dart';
 import 'register.dart';
 import 'search.dart';
 
-// TODO: Convert ShrineApp to stateful widget (104)
 class BulletinApp extends StatelessWidget {
   const BulletinApp({Key? key}) : super(key: key);
 
@@ -32,11 +17,7 @@ class BulletinApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Shrine',
-        // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
         home: LoginPage(),
-        // TODO: Make currentCategory field take _currentCategory (104)
-        // TODO: Pass _currentCategory for frontLayer (104)
-        // TODO: Change backLayer field value to CategoryMenuPage (104)
         initialRoute: '/login',
         routes:{
           '/home':(context)=> LoginPage(),
@@ -49,7 +30,6 @@ class BulletinApp extends StatelessWidget {
           '/editstory':(context) => editPage(post: Story(likeUserList: [], content: '', updatetime: '', uid: '', contentimgUrl: '', likeUsers: 0, docid: '', username: '', userimgUrl: '')),
 
         }
-      // TODO: Add a theme (103)
     );
   }
 }
